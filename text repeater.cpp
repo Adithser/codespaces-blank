36dc9ee6468402f0814b39d8560c371ed3e1cb94
Adithser <101507555+Adithser@users.noreply.github.com>
2023-05-08 06:39:37 +0000

    first commit

---
+++ text repeater.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+        int i, a,n;
+            string z,c,h;
+                string m = "welcome to the text repeater , type Ok to continue";
+                    string f = "Here you can repeat your text and have some fun :) ,type Ok to continue";
+                        string t = "Ok";
+                            string g = "Ok";
+                                cout<<m;
+                                    cin>>z;
+                                        //1st phase
+                                            for(i=1; i<=4; i++)
+                                                {
+                                                            if(z!=t)
+                                                                    {
+                                                                                    cout<<"🔴 error : type Ok to continue 🔴";
+                                                                                                cin>>z;
+                                                                    }
+                                                }
+                                                    if(z==t)
+                                                        {
+                                                                    cout<<f;
+                                                                            cin>>h;
+                                                        }
+                                                            //2nd phase
+                                                                for(i=1; i<=4; i++)
+                                                                    {
+                                                                                if(h!=t)
+                                                                                        {
+                                                                                                        cout<<"🧿 error : type Ok to continue 🧿";
+                                                                                                                    cin>>h;
+                                                                                        }
+                                                                    }
+                                                                        if(h==t)
+                                                                            {
+                                                                                        cout<<"which word do you want to repeat ?";
+                                                                                                cin>>c;
+                                                                            }
+                                                                                //3rd phase
+                                                                                    cout<<"how many time do you want to repeat?";
+                                                                                        cin>>n;
+                                                                                            //4th phase
+                                                                                                for(a=1; a<=n; a++)
+                                                                                                    {
+                                                                                                                cout<<c<<"\n";
+
+                                                                                                    }
+}#include<iostream>
+using namespace std;
+int main ()
+{
+        int i, a,n;
+            string z,c,h;
+                string m = "welcome to the text repeater , type Ok to continue";
+                    string f = "Here you can repeat your text and have some fun :) ,type Ok to continue";
+                        string t = "Ok";
+                            string g = "Ok";
+                                cout<<m;
+                                    cin>>z;
+                                        //1st phase
+                                            for(i=1; i<=4; i++)
+                                                {
+                                                            if(z!=t)
+                                                                    {
+                                                                                    cout<<"🔴 error : type Ok to continue 🔴";
+                                                                                                cin>>z;
+                                                                    }
+                                                }
+                                                    if(z==t)
+                                                        {
+                                                                    cout<<f;
+                                                                            cin>>h;
+                                                        }
+                                                            //2nd phase
+                                                                for(i=1; i<=4; i++)
+                                                                    {
+                                                                                if(h!=t)
+                                                                                        {
+                                                                                                        cout<<"🧿 error : type Ok to continue 🧿";
+                                                                                                                    cin>>h;
+                                                                                        }
+                                                                    }
+                                                                        if(h==t)
+                                                                            {
+                                                                                        cout<<"which word do you want to repeat ?";
+                                                                                                cin>>c;
+                                                                            }
+                                                                                //3rd phase
+                                                                                    cout<<"how many time do you want to repeat?";
+                                                                                        cin>>n;
+                                                                                            //4th phase
+                                                                                                for(a=1; a<=n; a++)
+                                                                                                    {
+                                                                                                                cout<<c<<"\n";
+
+                                                                                                    }
+}
+                                                                                                    }
+                                                                            }
+                                                                                        }
+                                                                    }
+                                                        }
+                                                                    }
+                                                }
+}
+                                                                                                    }
+                                                                            }
+                                                                                        }
+                                                                    }
+                                                        }
+                                                                    }
+                                                }
+}
